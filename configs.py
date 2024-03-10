28010717b86e7b530f2a44452865340a5de3d430
Abhishekk6202 <161255087+Abhishekk6202@users.noreply.github.com>
2024-03-10 12:35:22 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", ""))
-  API_HASH = os.environ.get("API_HASH", "")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", ""))
-  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "MoneyKamalo.com")
-  SHORTLINK_API = os.environ.get('SHORTLINK_API', "0eefb93e1e3ce9470a7033115ceb1bad13a9d674")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", ""))
-  DATABASE_URL = os.environ.get("DATABASE_URL", "")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+  API_ID = int(os.environ.get("API_ID", "21414727"))
+  API_HASH = os.environ.get("API_HASH", "b4135f6b8cd476d931e78787a0cb77c1")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "7045910445:AAGYLecj9xnDoXKzDjnNvb9Pp2iMkKwRKbw")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "Mr_AbhiiStore_bot")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002084771326"))
+  SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "ziplinker.net")
+  SHORTLINK_API = os.environ.get('SHORTLINK_API', "8a600b4f9a84a1ea10f62afc0813bf3601d990e0")
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6796937630"))
+  DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://abhishekkumar876:passpmongodb@cluster0.xobjcca.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001998607849")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002013600080"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -50,4 +50,6 @@ How to Use Bot & it's Benefits??
 📢 Send me any File & It will be uploaded in My Database & You will Get the File Link.
 
 ⚠️ Benefits: If you have a TeleGram Movie Channel or Any Copyright Channel, Then Its Useful for Daily Usage, You can Send Me Your File & I will Send Permanent Link to You & Channel will be Safe from **CopyRight Infringement** Issue. I support Channel Also You Can Check **About Bot**.
+
+❌ PORNOGRAPHY CONTENTS are strictly prohibited & get Permanent Ban.
 """
